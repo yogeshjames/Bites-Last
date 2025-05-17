63af05290431423010e5367315239fc7f2dcd227
yogeshjames <150313150+yogeshjames@users.noreply.github.com>
2025-05-17 21:42:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000', 
     'http://localhost:3001',
+    'https://bites-hotel-nwjp1sj5w-yogeshjames-projects.vercel.app/',
     'https://bites-hotel.vercel.app/'////prod domain
   ],
   credentials: true
